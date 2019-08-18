d472b51d073fc92490187d3687b209106970b906
aashish2000 <aash.ananth@gmail.com>
2019-08-18 11:53:21 +0530

    FTP Code Correction

--- ftpclient.c
+++ ftpclient.c
@@ -1,25 +1,31 @@
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<stdio.h>
-#define SA struct sockaddr
+#include<strings.h>
+#include<stdlib.h>
 int main(int argc,char**argv)
 {
-int sockfd;
-char fname[25];
-int len;
-struct sockaddr_in servaddr,cliaddr;
-sockfd=socket(AF_INET,SOCK_STREAM,0);
-bzero(&servaddr,sizeof(servaddr));
-servaddr.sin_family=AF_INET;
-servaddr.sin_addr.s_addr=inet_addr("10.6.12.5");
-servaddr.sin_port=htons(atoi(argv[1]));
-printf("Works till here");
-inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
-connect(sockfd,(SA*)&servaddr,sizeof(servaddr));
-char buffer[100];
-FILE *f;
-f=fopen("newfile.txt","r");
-fscanf(f,"%s",buffer);
-write(sockfd,buffer,100);
-printf("the file was sent successfully");
+	int sockfd;
+	char fname[25];
+	int len;
+	printf("\n Enter the IP address of the server: ");
+	char ipaddr[30];
+	scanf("%s",ipaddr);
+	struct sockaddr_in servaddr,cliaddr;
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(ipaddr);
+	servaddr.sin_port = htons(atoi(argv[1]));
+	inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+	connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+	char buffer[100];
+	FILE *f;
+	printf("\n Enter filename to send: ");
+	char filesend[30];
+	scanf("%s", filesend);
+	f = fopen(filesend,"r");
+	fscanf(f,"%s",buffer);
+	write(sockfd,buffer,100);
+	printf("The file was sent successfully! \n");
 }
